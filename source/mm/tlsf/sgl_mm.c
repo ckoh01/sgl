1d6a710b14fe0173948e5c6dc46f7f233feb6858
LiShanwenGit <1477153217@qq.com>
2026-05-16 13:37:26 +0800

    fix: tlsf memmory monitor issue

--- source/mm/tlsf/sgl_mm.c
+++ source/mm/tlsf/sgl_mm.c
@@ -2,9 +2,9 @@
  *
  * MIT License
  *
- * Copyright(c) 2023-present All contributors of SGL  
+ * Copyright(c) 2023-present All contributors of SGL
  * Document reference link: https://sgl-docs.readthedocs.io
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
@@ -16,10 +16,13 @@
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * AUTHORS OR COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #include "tlsf.h"
@@ -28,101 +31,81 @@
 #include <sgl_log.h>
 #include <sgl_cfgfix.h>
 
-
 static tlsf_t mem_tlsf;
-static sgl_mm_monitor_t mem = {
-    .total_size = 0,
-    .free_size = 0,
-    .used_size = 0,
-};
+static size_t g_total_pool_size = 0;
 
+typedef struct {
+    size_t free_bytes;
+} tlsf_walk_ctx_t;
+
+static void tlsf_walk_callback(void* ptr, size_t size, int used, void* user)
+{
+    SGL_UNUSED(ptr);
+    tlsf_walk_ctx_t* ctx = (tlsf_walk_ctx_t*)user;
+    if (!used) {
+        ctx->free_bytes += size;
+    }
+}
 
 /**
  * @brief  initialize memory pool
- * @param  mem_start  start address of memory pool
- * @param  len  length of memory pool
  */
 void sgl_mm_init(void *mem_start, size_t len)
 {
     mem_tlsf = tlsf_create_with_pool(mem_start, len);
-    mem.total_size += len;
+    g_total_pool_size = len;
 }
-
 
 /**
  * @brief  add memory pool
- * @param  mem_start  start address of memory pool
- * @param  len  length of memory pool
  */
 void sgl_mm_add_pool(void *mem_start, size_t len)
 {
     tlsf_add_pool(mem_tlsf, mem_start, len);
-    mem.total_size += len;
+    g_total_pool_size += len;
 }
 
-
-/**
- * @brief  memory alloc, the function is unsafe, you should ensure that 
- *         the requested size is smaller than the free size of memory
- * 
- * @param  size   request size of memory
- * 
- * @return point to request memory address
-*/
 void* sgl_malloc(size_t size)
 {
     void *ret = tlsf_malloc(mem_tlsf, size);
     if(ret == NULL) {
         SGL_LOG_ERROR("out of memory");
-        return NULL;
     }
-
-    mem.used_size += size;
     return ret;
 }
 
-
-/**
- * @brief  memory realloc, the function is unsafe, you should ensure that 
- *         the requested size is smaller than the free size of memory
- * @param  p      the pointer of request size of memory
- * @param  size   request size of memory
- */
 void* sgl_realloc(void *p, size_t size)
 {
     void *ret = tlsf_realloc(mem_tlsf, p, size);
     if(ret == NULL) {
         SGL_LOG_ERROR("out of memory");
-        return NULL;
     }
-
-    mem.used_size += size;
-
     return ret;
 }
 
-
-/**
- * @brief  memory free
- * 
- * @param  p the pointer of request size of memory
- * 
- * @return none
-*/
 void sgl_free(void *p)
 {
-    mem.used_size -= tlsf_block_size(p);
-
     tlsf_free(mem_tlsf, p);
 }
 
-
 sgl_mm_monitor_t sgl_mm_get_monitor(void)
 {
-    int integer = (mem.used_size * 100) / mem.total_size;
-    int decimal = (mem.used_size * 10000) / mem.total_size - (integer * 100);
-    mem.used_rate = integer << 8 | decimal;
-    mem.free_size = mem.total_size - mem.used_size;
+    static sgl_mm_monitor_t monitor;
+    tlsf_walk_ctx_t ctx = {0};
 
-    return mem;
+    if (mem_tlsf == NULL || g_total_pool_size == 0) {
+        return monitor;
+    }
+
+    tlsf_walk_pool(tlsf_get_pool(mem_tlsf), tlsf_walk_callback, &ctx);
+
+    monitor.total_size = g_total_pool_size;
+    monitor.free_size = ctx.free_bytes;
+    monitor.used_size = g_total_pool_size - ctx.free_bytes;
+
+    int integer = (monitor.used_size * 100) / monitor.total_size;
+    int decimal = ((monitor.used_size * 10000) / monitor.total_size) % 100;
+    monitor.used_rate = (integer << 8) | (decimal & 0xFF);
+
+    return monitor;
 }
